d46de106cedc6d088cfd130e9835614812b5b943
architchaturvedi24 <architchaturvedi24@gmail.com>
2026-05-30 18:40:20 +0530

    Datatypes summary

---
+++ javascript/datatypes_summary.js
@@ -0,0 +1,18 @@
+// Primitive 
+
+// 7 types : String ,Number, Boolean , Null,undefined ,symbol BigInt 
+
+const score = 100
+const scorevalue =100.3
+
+const isLogged = false 
+const outsidetemp = null
+let userEmail ;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+// Refrence (Non Primitive )
+
+// Array, Objects,Functions
